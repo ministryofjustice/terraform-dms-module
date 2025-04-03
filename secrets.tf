75f715af2c65151af8918e6c7734be1362297e62
Siva Bathina <siva.bathina@justice.gov.uk>
2025-04-03 16:29:15 +0100

    removed docker reference

--- secrets.tf
+++ secrets.tf
@@ -2,5 +2,6 @@
 # #trivy:ignore:AVD-AWS-0098: The secret is encrypted using an AWS managed key
 resource "aws_secretsmanager_secret" "dms_source" {
   #checkov:skip=CKV_AWS_149: The secret is encrypted using an AWS managed key
-  name = "${var.db}-source-${var.environment}"
+  name_prefix = "${var.db}-source-${var.environment}-"
+
 }
