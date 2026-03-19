dcddf8a1e2a301b1d3494f3c4bd2d73bbf571e77
AntonyJScott <112949130+AntonyJScott@users.noreply.github.com>
2026-03-19 09:33:46 +0000

    Dms module full load min change (#159)
    * min dms module changes made for independent full load
    
    * terraform-docs: automated action
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- metadata_generator.tf
+++ metadata_generator.tf
@@ -47,6 +47,29 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "validation_metada
   }
 }
 
+# Create security group for Lambda function
+#trivy:ignore:AVD-AWS-0104 Allow all egress traffic
+resource "aws_security_group" "metadata_generator_lambda_function" {
+  #checkov:skip=CKV_AWS_382: Allow all egress traffic
+  #checkov:skip=CKV2_AWS_5: Security Groups are attached to another resource
+  name        = "${var.db}-metadata-generator-lambda-function"
+  vpc_id      = var.vpc_id
+  description = "Security group for Lambda function to generate metadata for ${var.db} DMS data output"
+
+  egress {
+    description = "Allow all egress traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+locals {
+  mapping_rule_buckets = toset(concat(
+    [var.dms_mapping_rules.bucket],
+  [for _, job in var.independent_full_loads : job.path.bucket]))
+}
 
 data "aws_iam_policy_document" "metadata_generator_lambda_function" {
   # Lambda can upload files to the metadata bucket
@@ -96,10 +119,12 @@ data "aws_iam_policy_document" "metadata_generator_lambda_function" {
       "s3:GetObject",
     ]
 
-    resources = [
-      "arn:aws:s3:::${var.dms_mapping_rules.bucket}",
-      "arn:aws:s3:::${var.dms_mapping_rules.bucket}/*",
-    ]
+    resources = flatten([
+      for b in local.mapping_rule_buckets : [
+        "arn:aws:s3:::${b}",
+        "arn:aws:s3:::${b}/*",
+      ]
+    ])
   }
 
   # Lambda can reprocess data in the invalid bucket
@@ -175,24 +200,6 @@ data "aws_iam_policy_document" "metadata_generator_lambda_function" {
   }
 }
 
-# Create security group for Lambda function
-#trivy:ignore:AVD-AWS-0104 Allow all egress traffic
-resource "aws_security_group" "metadata_generator_lambda_function" {
-  #checkov:skip=CKV_AWS_382: Allow all egress traffic
-  #checkov:skip=CKV2_AWS_5: Security Groups are attached to another resource
-  name        = "${var.db}-metadata-generator-lambda-function"
-  vpc_id      = var.vpc_id
-  description = "Security group for Lambda function to generate metadata for ${var.db} DMS data output"
-
-  egress {
-    description = "Allow all egress traffic"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
 #trivy:ignore:AVD-AWS-0066 X-Ray tracing not currently required. Logs sent to CloudWatch.
 module "metadata_generator" {
   # Commit hash for v7.20.1
@@ -236,6 +243,65 @@ module "metadata_generator" {
     USE_GLUE_CATALOG                     = var.write_metadata_to_glue_catalog
     DMS_MAPPING_RULES_BUCKET             = var.dms_mapping_rules.bucket
     DMS_MAPPING_RULES_KEY                = var.dms_mapping_rules.key
+    RETRY_FAILED_AFTER_RECREATE_METADATA = var.retry_failed_after_recreate_metadata
+  }
+
+  source_path = [{
+    path = "${path.module}/lambda_functions/metadata_generator/"
+    commands = [
+      "pip3.12 install --platform=manylinux2014_x86_64 --only-binary=:all: --no-compile --target=. -r requirements.txt",
+      ":zip",
+    ]
+  }]
+
+  tags = var.tags
+}
+
+#trivy:ignore:AVD-AWS-0066 X-Ray tracing not currently required. Logs sent to CloudWatch.
+module "independent_metadata_generator" {
+  for_each = var.independent_full_loads
+  # Commit hash for v7.20.1
+  source = "git::https://github.com/terraform-aws-modules/terraform-aws-lambda?ref=84dfbfddf9483bc56afa0aff516177c03652f0c7"
+
+  function_name           = "${var.db}-metadata-generator-${each.value.full_load_name}"
+  description             = "Lambda to generate metadata for ${var.db} DMS data output"
+  handler                 = "main.handler"
+  runtime                 = "python3.12"
+  memory_size             = 4096
+  timeout                 = 900
+  architectures           = ["x86_64"]
+  build_in_docker         = false
+  store_on_s3             = true
+  s3_bucket               = aws_s3_bucket.lambda.bucket
+  s3_object_storage_class = "STANDARD"
+  s3_prefix               = "metadata-generator/${each.value.full_load_name}/"
+  hash_extra              = each.value.full_load_name
+
+  # Lambda function will be attached to the VPC to access the source database
+  vpc_security_group_ids = [aws_security_group.metadata_generator_lambda_function.id]
+  vpc_subnet_ids         = data.aws_subnets.subnet_ids_vpc_subnets.ids
+  attach_network_policy  = true
+
+
+  create_role = false
+  lambda_role = module.metadata_generator.lambda_role_arn
+
+  environment_variables = {
+    ENVIRONMENT                          = var.environment
+    DB_SECRET_ARN                        = var.dms_source.secrets_manager_arn
+    METADATA_BUCKET                      = aws_s3_bucket.validation_metadata.bucket
+    LANDING_BUCKET                       = aws_s3_bucket.landing.bucket
+    INVALID_BUCKET                       = aws_s3_bucket.invalid.bucket
+    RAW_HISTORY_BUCKET                   = local.raw_history_bucket_id
+    OUTPUT_KEY_PREFIX                    = var.output_key_prefix
+    LAMBDA_BUCKET                        = aws_s3_bucket.lambda.bucket
+    ENGINE                               = var.dms_source.engine_name
+    DATABASE_NAME                        = var.dms_source.sid
+    GLUE_CATALOG_ARN                     = var.glue_catalog_arn
+    GLUE_CATALOG_ROLE_ARN                = var.glue_catalog_role_arn
+    USE_GLUE_CATALOG                     = var.write_metadata_to_glue_catalog
+    DMS_MAPPING_RULES_BUCKET             = each.value.path.bucket
+    DMS_MAPPING_RULES_KEY                = each.value.path.key
     RETRY_FAILED_AFTER_RECREATE_METADATA = var.retry_failed_after_recreate_metadata
   }
 
